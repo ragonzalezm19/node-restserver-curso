4662c5c830eb2e4e0f3700e4da61a2a9d8c44f01
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2020-06-23 23:57:47 -0400

    Npm start listo

--- server/server.js
+++ server/server.js
@@ -11,33 +11,33 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+  res.json('get Usuario')
 })
 
 app.post('/usuario', function(req, res) {
-    let body = req.body;
+  let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    }
-
-    res.json({
-        persona: body
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario'
     })
+  }
+
+  res.json({
+    persona: body
+  })
 })
 
 app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json('put Usuario ' + id)
+  let id = req.params.id;
+  res.json('put Usuario ' + id)
 })
 
 app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+  res.json('delete Usuario')
 })
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando pruerto ' + process.env.PORT)
+  console.log('Escuchando pruerto ' + process.env.PORT)
 })
