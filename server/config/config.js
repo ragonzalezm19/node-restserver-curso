ec3b395f781369bc42c5653ee5ea717724ce9c4f
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2020-06-26 23:08:09 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-  urlDB = 'mongodb+srv://ragonzalezm19:8nIPZcgggu8J8g1L@cluster0.becd7.mongodb.net/cafe'
+  urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB
